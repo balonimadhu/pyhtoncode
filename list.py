a228fe81a45ed9336241fef3697b5e1988048c87
balonimadhu <balonimadhu@gmail.com>
2022-02-24 19:17:11 +0530

    list file has been pushed

---
+++ list.py
@@ -0,0 +1,16 @@
+#list
+list = ["madhu", "kiran", "diya", "anshika", "vaggeisha"]
+print(list)
+#Access particular one value
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[-2])
+#sort function
+list.sort()
+print(list)
+#reverse function
+list.reverse()
+print(list)
---
+++ python-2.py
@@ -0,0 +1,6 @@
+#sum of two number
+num1=int(input("enter your first number:"))
+num2=int(input("enter your second number:"))
+sum=num1+num2
+print("sum:",sum)
+
